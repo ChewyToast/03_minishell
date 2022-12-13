70b2fbc5c9993469385e99cbc6a08d30cd257086
ChewyToast <moll.bruno.99@gmail.com>
2022-12-13 23:47:37 +0100

    lets start parsing

--- inc/headers/minishell.h
+++ inc/headers/minishell.h
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmoll-pe <bmoll-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/12 13:06:45 by bruno             #+#    #+#             */
-/*   Updated: 2022/12/13 23:40:21 by bmoll-pe         ###   ########.fr       */
+/*   Created: 2022/12/13 23:29:10 by bmoll-pe          #+#    #+#             */
+/*   Updated: 2022/12/13 23:36:57 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "structs.h"
-#include "minishell.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-int	main(int argc, char **argv, char **env)
-{
-	t_master	master;
-	t_bracket	*bracket;
-	if (argc != 1)
-		return (0);
-	master.env = env;
-	master.exit_code = 0;
-	while (1)
-	{
-		if (parser(&master, &bracket, readline("ba.sh $ ")))
-			return (1);
-		// printf("->%s<-\n", input);
-	}
-	(void)argv;
-	(void)env;
-	exit (master.exit_code);
-}
+//	---- parser.c
+_Bool	parser(t_master *master, t_bracket **bracket, char *input);
+
+#endif
