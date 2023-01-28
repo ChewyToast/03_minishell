85696b51c8078d03ce0affe5986bd963ffe3b2e0
ailopez-o <aitorlpzaudikana@gmail.com>
2023-01-28 13:14:32 +0100

    tilde expansor working

--- src/executor/expander.c
+++ src/executor/expander.c
@@ -21,6 +21,7 @@ char	**expander(char *data, t_master *master)
 	int		pos;
 	char	*word;
 	char	*expanded;
+	char	*home_path;
 
 	if (!data)
 		return (NULL);
@@ -48,7 +49,7 @@ char	**expander(char *data, t_master *master)
 				tokens[0] = ft_strjoin_free (tokens[0], expanded);
 			data = data + pos;
 		}
-		else if ((*data) == '*' && !is_quoted)
+		else if ((*data) == '*' && !is_quoted && !is_dbl_quoted)
 		{
 			data = get_word_init(data, full_data);
 			word = ft_substr(data, 0, get_word_end(data) - data);
@@ -56,159 +57,39 @@ char	**expander(char *data, t_master *master)
 			free (full_data);
 			return (expand_wildcard(word));
 		}
-		else if ((*data) == '~')
+		else if ((*data) == '~' && !is_quoted && !is_dbl_quoted)
 		{
-			if (*(data - 1) == ' ')
+			if (ft_strlen(full_data) == 1)
 			{
-				expanded = env_get_value(master->env_list, "HOME");
-				if (expanded == NULL)
-					expanded = ft_strdup(master->tild_value);
-				tokens[0] = ft_strjoin_free (tokens[0], expanded);
+				home_path = env_get_value(master->env_list, "HOME");
+				if (home_path == NULL)
+					home_path = ft_strdup(master->tild_value);
+				tokens[0] = ft_strjoin_free(tokens[0], home_path);
 				data++;
+			}
+			else if ((data + 1) && *(data + 1))
+			{
+				expanded = ft_substr(data, 1, ft_strlen(data) - 1);
+				expanded = ft_strjoin_free(ft_strdup("/Users/"), expanded);
+				if (access(expanded, F_OK))
+					tokens[0] = ft_chrjoin(tokens[0], *(data++));
+				else
+				{
+					tokens[0] = ft_strjoin_free(tokens[0], expanded);
+					pos = get_word_end(data) - data;
+					data = data + pos;
+				}
 			}
 			else
 				tokens[0] = ft_chrjoin(tokens[0], *(data++));
 		}
 		else
-			tokens[0] = ft_chrjoin(tokens[0], *(data++));		
+			tokens[0] = ft_chrjoin(tokens[0], *(data++));
 	}
 	tokens[1] = NULL;
 	free (full_data);
-	return (tokens);	
+	return (tokens);
 }
-
-
-// char	*expander(char *data, t_master *master)
-// {
-// 	char	*new_data;
-// 	char	*expanded;
-// 	char	*word;
-// 	bool	is_quoted;
-// 	bool	is_dbl_quoted;
-// 	int		pos;
-// 	char	*full_data;
-// 	char	*word_init;
-// 	int		to_delete;
-// 	char	*temp;
-// 	char	*redirect_data;
-	
-// 	(void)temp;
-// 	(void)redirect_data;
-// 	full_data = data;
-// 	new_data = ft_strdup("");
-// 	while (*data)
-// 	{
-// 		if (*(data) == 92 && !is_quoted)
-// 		{
-// 			new_data = ft_chrjoin(new_data, *(++data));
-// 			data++;
-// 		}
-// 		if ((*data) == 39)
-// 		{
-// 			is_quoted = !is_quoted;
-// 			data++;
-// 		}
-// 		else if ((*data) == 34)
-// 		{
-// 			is_dbl_quoted = !is_dbl_quoted;
-// 			data++;
-// 		}
-// 		else if (!is_quoted && !is_dbl_quoted && *data == ' ' && (*(data + 1) == ' ' || *(data + 1) == '\0'))
-// 			data++;
-// 		else if (*data == ' ' && (*(data + 1) == '>' || *(data + 1) == '<') && !is_quoted && !is_dbl_quoted)
-// 			data++;
-// 		// else if (((*data) == '>' || (*data) == '<') && !is_quoted && !is_dbl_quoted)
-// 		// {
-// 		// 	data++;
-// 		// 	while (*(data) == ' ' && *(data - 1) != 39 && *(data - 1) != 34)
-// 		// 		data++;
-// 		// 	pos = get_redirect_end(data) - (data);
-// 		// 	word = ft_substr(data, 0, pos - 1);
-// 		// 	word = expand_data(word, node, master);
-// 		// 	add_new_redirect(node, word, *data);
-// 		// 	data = data + pos;
-// 		// }	
-// 		else if ((*data) == '$' && !is_quoted)
-// 		{
-// 			data++;
-// 			pos = get_word_end(data) - data;
-// 			word = ft_substr(data, 0, pos);
-// 			expanded = env_get_value(master->env_list, word);
-// 			free (word);
-// 			if (expanded != NULL)
-// 				new_data = ft_strjoin_free (new_data, expanded);
-// 			data = data + pos;
-// 		}		
-// 		else if ((*data) == '*' && !is_quoted)
-// 		{
-// 			word_init = get_word_init(data, full_data);
-// 			to_delete = data - word_init;
-// 			temp = ft_substr(new_data, 0, ft_strlen(new_data) - to_delete);
-// 			free (new_data);
-// 			new_data = temp;
-// 			data = get_word_init(data, full_data);
-// 			pos = get_word_end(data) - data;
-// 			word = ft_substr(data, 0, pos);
-// 			expanded = expand_str_wildcard(word);
-// 			new_data = ft_strjoin_free (new_data, expanded);
-// 			data = data + pos;
-// 		}
-// 		else if ((*data) == '~')
-// 		{
-// 			if (*(data - 1) == ' ')
-// 			{
-// 				expanded = env_get_value(master->env_list, "HOME");
-// 				if (expanded == NULL)
-// 					expanded = ft_strdup(master->tild_value);
-// 				new_data = ft_strjoin_free (new_data, expanded);
-// 				data++;
-// 			}
-// 			else
-// 				new_data = ft_chrjoin(new_data, *(data++));
-// 		}
-// 		else
-// 			new_data = ft_chrjoin(new_data, *(data++));
-// 	}
-// 	free (full_data);
-// 	return (new_data);
-// }
-
-// char	**get_tokens(char *data)
-// {
-// 	char **tokens;
-// 	char *last_token;
-// 	char *token;
-// 	int	num_tokens;
-	
-// 	tokens = malloc (sizeof (char *));
-// 	if (tokens == NULL)
-// 		return (NULL);
-// 	last_token = data;
-// 	num_tokens = 0;
-// 	while(*data)
-// 	{
-// 		if ((*data == ' '  &&  *(data - 1) != ' ') || *(data + 1) == '\0')
-// 		{
-// 			if (*(data + 1) == '\0')
-// 				data++;
-// 			token = ft_substr(last_token, 0, data - last_token);
-// 			if (num_tokens == 0)
-// 				token = ft_substr(token, 0, ft_strchr(token, ' ') - token);
-// 			num_tokens++;
-// 			tokens = ft_realloc(tokens, sizeof (char *) * (num_tokens + 1));
-// 			if (tokens == NULL)
-// 				return (NULL);
-// 			tokens[num_tokens - 1] = token;
-// 			last_token = data + 1;
-// 		}
-// 		if (*(data) == '\0')
-// 			break;
-// 		data++;
-// 	}
-// 	tokens[num_tokens] = NULL;
-// 	return (tokens);
-// }
-
 
 bool is_word_limit(char c)
 {
@@ -239,8 +120,6 @@ char	*get_word_end(char *data)
 		data++;
 	return (data);
 }
-
-
 
 char	*get_word_init(char *data, char *data_min)
 {
