30c2111895e68111a5da84dfc4c748499ce87e16
ChewyToast <moll.bruno.99@gmail.com>
2023-03-08 05:02:02 +0100

    starting redirects again

--- src/executor/redirects.c
+++ src/executor/redirects.c
@@ -6,7 +6,7 @@
 /*   By: bmoll-pe <bmoll-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 10:01:08 by test              #+#    #+#             */
-/*   Updated: 2023/03/07 18:36:08 by bmoll-pe         ###   ########.fr       */
+/*   Updated: 2023/03/08 04:56:18 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,21 @@ _Bool	prepare_redirect(t_redirect *redi)
 	while (redi)
 	{
 		if (redi->type == RDOC)
-			if (own_here_doc(&tmp_fd, redi))
-				return (print_error(ft_strjoin("ba.sh: ", strerror(errno)), 1));// falta cerrar todo!@to_do
+		{
+			own_here_doc(&tmp_fd, redi);
+			// return (print_error(ft_strjoin("ba.sh: ", strerror(errno)), 1));// falta cerrar todo!@to_do
+		}
 		if (redi->type != RDOC)
-			if (!prepare_fd(&tmp_fd, redi->data, redi->type))
-				if (dup2(tmp_fd, redi->fd) < 0 || (close_fdman(&fdman, redi->fd))
-					|| (add_fdman(&fdman, redi->fd, tmp_fd)))// no estoy seguro de si esto esta bien @to_do
-					return (print_error(ft_strjoin("ba.sh: ", strerror(errno)), 1));
+		{
+			prepare_fd(&tmp_fd, redi->data, redi->type);
+		}
+		if (tmp_fd > 0)
+		{
+			if (dup2(tmp_fd, redi->fd) < 0)
+				return (print_error(ft_strjoin("ba.sh: ", strerror(errno)), 1));
+			if ((close_fdman(&fdman, redi->fd)) || (add_fdman(&fdman, redi->fd, tmp_fd)))// no estoy seguro de si esto esta bien @to_do
+				return (print_error(ft_strjoin("ba.sh: ", strerror(errno)), 1));
+		}
 		redi = redi->next;
 	}
 	return (0);
@@ -90,26 +98,28 @@ static bool	prepare_fd(int *fd, char *data, int8_t type)
 {
 	if (type == RIN)
 	{
-		if (access(data, F_OK))
-			return (print_error(ft_strjoin("ba.sh: ", ft_strjoin(data, ": No such a file or directory\n")), 1));
-		if (access(data, R_OK))
-			return (print_error(ft_strjoin("ba.sh: ", ft_strjoin(data, ": Permision denied\n")), 1));
+		// if (access(data, F_OK))
+		// 	return (print_error(ft_strjoin("ba.sh: ", ft_strjoin(data, ": No such a file or directory\n")), 1));
+		// if (access(data, R_OK))
+		// 	return (print_error(ft_strjoin("ba.sh: ", ft_strjoin(data, ": Permision denied\n")), 1));
 		*fd = open(data, O_RDONLY);
 	}
 	else if (type == ROUT)
 	{
-		if (!access(data, F_OK))
-			if (access(data, W_OK))
-				return (print_error(ft_strjoin("ba.sh: ", ft_strjoin(data, ": Permision denied\n")), 1));
+		// if (!access(data, F_OK))
+		// 	if (access(data, W_OK))
+		// 		return (print_error(ft_strjoin("ba.sh: ", ft_strjoin(data, ": Permision denied\n")), 1));
 		*fd = open(data, O_CREAT | O_WRONLY | O_TRUNC, 0666);
 	}
 	else if (type == RADD)
 	{
-		if (!access(data, F_OK))
-			if (access(data, W_OK))
-				return (print_error(ft_strjoin("ba.sh: ", ft_strjoin(data, ": Permision denied\n")), 1));
+		// if (!access(data, F_OK))
+		// 	if (access(data, W_OK))
+		// 		return (print_error(ft_strjoin("ba.sh: ", ft_strjoin(data, ": Permision denied\n")), 1));
 		*fd = open(data, O_CREAT | O_WRONLY | O_APPEND, 0666);
 	}
+	if (*fd < 0)
+		printf("bash: %s: %s\n", data, strerror(errno));
 	return (0);
 }
 
@@ -142,6 +152,7 @@ static bool	own_here_doc_while(int *fd, char *limitator)
 
 	if (close(fd[0]) < 0)
 		return (1);//ERROR!
+	write(2, limitator, ft_strlen(limitator));
 	while (42)
 	{
 		init_signals(HERE_DOC);
